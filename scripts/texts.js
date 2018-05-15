ea99c7ed3480f169a06d616c372be98143c5558d
ervan <ervan.dutheil@yahoo.fr>
2018-05-15 15:36:17 +0200

    bg

--- scripts/texts.js
+++ scripts/texts.js
@@ -1,3 +1,18 @@
 texts = {
-  "TestText":{"text":["I m not a potato","I am a navet"], "event" : "log", "event_args" : "stringP"}
+  "TestText":{"text":["I m not a potato","I am a navet"], "event" : "log", "event_args" : "stringP"},
+  "TestChoices":[
+      {"text": "Choice 1", "event": "log", "event_args": "Choice 1 arg"},
+      {
+          "text": "Choice 2",
+          "event": "choice",
+          "event_args": {
+              "text": "What will you choose?",
+              "choices": [
+                  {"text": "Choice 1", "event": "log", "event_args": "Choice 1 arg"},
+                  {"text": "Choice 2", "event": "log", "event_args": "Choice 2 arg"},
+              ]
+          }
+      },
+      {"text": "Choice 3", "event": "log", "event_args": "Choice 3 arg"},
+  ]
 }
