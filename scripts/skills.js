4aa50889f5c3b66509dae019240ee24886256aa6
Ivan Armagnat <i.armagnat@it-akademy.fr>
2018-05-18 12:05:33 +0200

    A much better skill management system

--- scripts/skills.js
+++ scripts/skills.js
@@ -2,8 +2,8 @@ const soundHelper = require("./helpers/soundHelper.js")
 
 function SkillStore() {
     this.list = {
-        "punch": punch,
-        "brouette_fu": brouette_fu,
+        "punch": simpleSkill("getRndInteger(1, 4) + str - def", './assets/sounds/argargarhh.wav'),
+        "brouette_fu": simpleSkill("getRndInteger(0, Math.floor(str / 2 + 1)) + def", './assets/sounds/brouetteHit.mp3'),
     }
     this.createSkillButtons = function () {
         for (let key in this.list) {
@@ -12,26 +12,17 @@ function SkillStore() {
     }
 }
 
-function punch(self, target) {
-    const str = self.getStats().str
-    const def = target.getStats().def
-    const dmg = getRndInteger(1, 4) + str - def
-    if (target.hp - dmg > 0) {
-        soundHelper.playSfx('./assets/sounds/argargarhh.wav')
+function simpleSkill(equation, sfx) {
+    return function (self, target) {
+        const str = self.getStats().str
+        const def = target.getStats().def
+        const dmg = eval(equation)
+        if (target.hp - dmg > 0) {
+            soundHelper.playSfx(sfx)
+        }
+        target.changeHp(dmg >= 0 ? -dmg : 0)
+        return dmg >= 0 ? dmg : 0
     }
-    target.changeHp(dmg >= 0 ? -dmg : 0)
-    return dmg >= 0 ? dmg : 0
-}
-
-function brouette_fu(self, target) {
-    const str = self.getStats().str
-    const def = target.getStats().def
-    const dmg = getRndInteger(0, Math.floor(str / 2 + 1)) + def
-    if (target.hp - dmg > 0) {
-        soundHelper.playSfx('./assets/sounds/brouetteHit.mp3')
-    }
-    target.changeHp(dmg >= 0 ? -dmg : 0)
-    return dmg >= 0 ? dmg : 0
 }
 
 const skillStore = new SkillStore()
